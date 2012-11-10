1ef910538e26f7b48a6b50d0b1440c9436f8c7b2
David Cameron <david.l.cameron@gmail.com>
2012-11-09 20:42:40 -0500

    Added Q promises, firing DB insert once DB is opened and jsdom has finished

--- src/models/tip.js
+++ src/models/tip.js
@@ -1,5 +1,27 @@
 var jsdom = require('jsdom');
 var mongoose = require('mongoose');
+var Q = require("q");
+var domDeferred = Q.defer();
+var dbDeferred = Q.defer();
+
+Q.all([domDeferred.promise, dbDeferred.promise]).spread(function (data) {
+	
+	var skillSchema = new mongoose.Schema({
+		niceName: String,
+		skillPointCost: Number,
+		rechargeTime: Number,
+		slug: String
+	});
+
+	var Skill = db.model('Skill', skillSchema);
+	var skill = new Skill(data);
+
+	skill.save(function () {
+		Skill.find(function (err, skills) {
+			console.log(skills);
+		});
+	});
+});
 
 jsdom.env({
 	html: 'http://wiki.guildwars2.com/wiki/Devourer_Venom',
@@ -11,7 +33,6 @@ jsdom.env({
 		var $infobox = $("div.infobox").first();
 		var $stats = $infobox.find('div.statistics span');
 
-
 		data.rechargeTime = $($stats[0]).text();
 		data.skillPointCost = $($stats[1]).text();
 
@@ -19,26 +40,14 @@ jsdom.env({
 		var pathname = window.location.pathname;
 		data.slug = pathname.substr(6); // everything after /wiki/
 
-		var db = mongoose.createConnection('localhost', 'gw2tips');
-
-		db.on('error', console.error.bind(console, 'connection error:'));
-
-		db.once('open', function () {
-			var skillSchema = new mongoose.Schema({
-				niceName: String,
-				skillPointCost: Number,
-				rechargeTime: Number,
-				slug: String
-			});
-
-			var Skill = db.model('Skill', skillSchema);
-			var skill = new Skill(data);
-
-			skill.save(function () {
-				Skill.find(function (err, skills) {
-					console.log(skills);
-				});
-			});
-		});
+		domDeferred.resolve(data);
 	}
+});
+
+var db = mongoose.createConnection('localhost', 'gw2tips');
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function () {
+	dbDeferred.resolve();
 });
