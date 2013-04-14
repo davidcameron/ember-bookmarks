7e8d3fe19193bf825666cf769431486247356496
David Cameron <david.l.cameron@gmail.com>
2013-04-14 19:09:21 -0400

    Using express's built in methods for serving static files and getting request params. Refactor for #16

---
+++ app.js
@@ -0,0 +1,50 @@
+var express = require('express'),
+	app = express(),
+	server = require('http').createServer(app),
+//	io = require('socket.io').listen(server),
+	fs = require('fs'),
+	util = require('util'),
+	create = require('./src/models/create'),
+	read = require('./src/models/read'),
+	destroy = require('./src/models/destroy'),
+	api = require('.//src/api');
+
+app.use(express.static(__dirname + '/public'));
+
+app.use('/media', express.static(__dirname + '/media'));
+
+app.get('/api/sites', function (req, res) {
+	api.read(['sites']).then(function (json) {
+		res.send(json);	
+	});
+});
+
+app.listen(8080);
+
+/*
+io.set('log level', 1);
+
+server.listen(8080);
+
+io.sockets.on('connection', function (socket) {
+	read.read().then(function (items) {
+		socket.emit('sites:all', items);
+	});
+
+	socket.on('create:sites', function (data) {
+		create.create(data)
+			.then(read.read)
+			.then(function (item) {
+				socket.emit('sites:one', item);
+			});
+	});
+
+	socket.on('destroy:site', function (data) {
+		destroy.destroy(data.url);
+	});
+
+	socket.on('update:site', function (data) {
+		update.update(data.query, data.data);
+	});
+});
+*/
