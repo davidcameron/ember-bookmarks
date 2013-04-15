26e4e0c6c8362848be04bb773cd4f72d0d73eef9
David Cameron <david.l.cameron@gmail.com>
2013-04-14 20:19:21 -0400

    Moved sites api to its own file. Refactor for #19

--- app.js
+++ app.js
@@ -1,57 +1,21 @@
 var express = require('express'),
 	app = express(),
-	server = require('http').createServer(app),
-//	io = require('socket.io').listen(server),
-	fs = require('fs'),
-	util = require('util'),
-	create = require('./src/models/create'),
-	read = require('./src/models/read'),
-	destroy = require('./src/models/destroy'),
-	api = require('.//src/api');
+	sites = require('./src/models/sites');
 
 app.use(express.bodyParser());
 app.use(express.static(__dirname + '/public'));
 app.use('/media', express.static(__dirname + '/media'));
 
 app.get('/api/sites', function (req, res) {
-	api.read(['sites']).then(function (json) {
-		res.send(json);	
+	sites.findAll().then(function (json) {
+		res.send({sites: json});
 	});
 });
 
 app.post('/api/sites', function (req, res) {
-	console.log('POST: /api/sites', req.body);
-	create.create(req.body).then(function (data) {
-		res.send(data);
+	sites.create(req.body).then(function (data) {
+		res.send({sites: data});
 	});
 });
 
 app.listen(8080);
-
-/*
-io.set('log level', 1);
-
-server.listen(8080);
-
-io.sockets.on('connection', function (socket) {
-	read.read().then(function (items) {
-		socket.emit('sites:all', items);
-	});
-
-	socket.on('create:sites', function (data) {
-		create.create(data)
-			.then(read.read)
-			.then(function (item) {
-				socket.emit('sites:one', item);
-			});
-	});
-
-	socket.on('destroy:site', function (data) {
-		destroy.destroy(data.url);
-	});
-
-	socket.on('update:site', function (data) {
-		update.update(data.query, data.data);
-	});
-});
-*/
---
+++ src/models/sites.js
@@ -0,0 +1,76 @@
+var mongo = require('mongoskin'),
+    Q = require("q"),
+    db = mongo.db('localhost/unminder', {safe: false}),
+    scraper = require('./fetch');
+
+db.collection('site').ensureIndex([['url', 1]], true);
+db.bind('site');
+
+// TODO fix this mess
+function findAll () {
+    var deferred = Q.defer();
+    db.site.find().toArray(function (err, items) {
+        items.map(function (el) {
+            el.id = el._id;
+            delete el.copy;
+            el.image = './media/screenshots/' + el.image + '.png';
+            return el;
+        });
+        deferred.resolve(items);
+    });
+
+    return deferred.promise;
+}
+
+function find (filter) {
+    var deferred = Q.defer();
+
+    db.site.find(filter).toArray(function (err, items) {
+        items.map(function (el) {
+            el.id = el._id;
+            delete el.copy;
+            el.image = './media/screenshots/' + el.image + '.png';
+            return el;
+        });
+        deferred.resolve(items);
+    });
+
+    return deferred.promise;
+}
+
+function create (data) {
+    var deferred = Q.defer();
+    data = data.site;
+    var url = data.url;
+    if (url.substring(0, 7) !== 'http://') {
+        url = 'http://' + url;
+    }
+
+    db.site.insert({url: url, category: data.category}, function (err, docs) {
+        console.log(docs);
+        if (err) {
+            console.log('insert error', err);
+            deferred.reject(err);
+        } else {
+
+            scraper.getSite(url).then(function (data) {
+                console.log("getSite resolved");
+                console.log("data: ", data);
+                db.site.update({_id: docs[0]._id}, {$set: {title: data.title, copy: data.copy, image: data.image}}, function (err) {
+                    console.log("update callback");
+                    if (err) {
+                        console.log(err);
+                        create.Deferred.reject(err);
+                    } else {
+                        deferred.resolve(docs);
+                    }
+                });
+            });
+        }
+    });
+
+    return deferred.promise;
+}
+
+exports.findAll = findAll;
+exports.create = create;
