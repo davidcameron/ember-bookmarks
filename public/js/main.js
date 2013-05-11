dcd6f494beafbeb12a7dc2e046171a2bb311b89d
David Cameron <david.l.cameron@gmail.com>
2013-05-11 18:05:41 -0400

    Added user model and signup controller to front-end

--- public/js/main.js
+++ public/js/main.js
@@ -1,166 +1,203 @@
-'use strict';
+/*global console, window, Ember, io, DS */
+(function () {
+    'use strict';
 
-var socket = io.connect();
+    var Unminder = Ember.Application.create(),
+        socket = io.connect();
 
-socket.on('connected', function () {
-    console.log('emitted connected');
-});
+    socket.on('connected', function () {
+        console.log('emitted connected');
+    });
 
-socket.on('posting', function () {
-    console.log('emitted posting');
-});
+    socket.on('posting', function () {
+        console.log('emitted posting');
+    });
 
-socket.on('create:site', function (data) {
-    console.log('create:site', data);
-    var id = data[0].id;
-    console.log('id:', id);
-    Unminder.Site.find(id).reload();
-    console.log(Unminder.Site.find(id));
-});
-
-var Unminder = Ember.Application.create();
-
-/* Ember Classes */
-
-Unminder.Router.map(function () {
-    this.resource('sites', {path: '/sites'});
-    this.resource('lists', {path: '/lists/:list_id'});
-    this.route('login');
-    this.route('signup');
-});
-
-/* Routes */
-
-Unminder.ApplicationRoute = Ember.Route.extend({
-    model: function () {
-        return Unminder.List.find();
-    }
-});
-
-Unminder.IndexRoute = Ember.Route.extend({
-    setupController: function (controller) {
-        controller.set('title', "Unminder Index Route!");
-    }
-});
-
-Unminder.SitesRoute = Ember.Route.extend({
-    model: function () {
-        return Unminder.Site.find();
-    }
-});
-
-Unminder.ListsRoute = Ember.Route.extend({
-    setupController: function (controller, model) {
-        controller.set('content', model.get('sites'));
-        controller.set('heading', model.get('title'));
-        controller.set('template', 'sites');
-    },
-    model: function (params) {
-        return Unminder.List.find(params.list_id);
-    }
-});
-
-/* Models */
-
-Unminder.Store = DS.Store.extend({
-    revision: 12
-});
-
-DS.RESTAdapter.reopen({
-    namespace: 'api'
-});
+    socket.on('create:site', function (data) {
+        console.log('create:site', data);
+        var id = data[0].id;
+        console.log('id:', id);
+        Unminder.Site.find(id).reload();
+        console.log(Unminder.Site.find(id));
+    });
 
 
-Unminder.Site = DS.Model.extend({
-    title: DS.attr('string'),
-    image: DS.attr('string'),
-    url: DS.attr('string'),
-    list: DS.belongsTo('Unminder.List'),
-    backgroundStyle: function () {
-        if (!this.get('image')) {
-            return '';
+    /* Ember Classes */
+
+    Unminder.Router.map(function () {
+        this.resource('sites', {path: '/sites'});
+        this.resource('lists', {path: '/lists/:list_id'});
+        this.route('login');
+        this.route('signup');
+    });
+
+    /* Routes */
+
+    Unminder.ApplicationRoute = Ember.Route.extend({
+        model: function () {
+            return Unminder.List.find();
         }
+    });
 
-        var backgroundImage = this.get('image'),
-            styleString = 'background-image: url(' + backgroundImage + ')';
-        return styleString;
-    }.property('image')
-});
+    Unminder.IndexRoute = Ember.Route.extend({
+        setupController: function (controller) {
+            controller.set('title', "Unminder Index Route!");
+        }
+    });
 
-Unminder.List = DS.Model.extend({
-    title: DS.attr('string'),
-    sites: DS.hasMany('Unminder.Site')
-});
+    Unminder.SitesRoute = Ember.Route.extend({
+        model: function () {
+            return Unminder.Site.find();
+        }
+    });
 
-Unminder.User = DS.Model.extend({
-    email: DS.attr('string'),
-    name: DS.attr('string'),
-    password: DS.attr('string')
-});
+    Unminder.ListsRoute = Ember.Route.extend({
+        setupController: function (controller, model) {
+            controller.set('content', model.get('sites'));
+            controller.set('heading', model.get('title'));
+            controller.set('template', 'sites');
+        },
+        model: function (params) {
+            return Unminder.List.find(params.list_id);
+        }
+    });
 
-/* Controllers */
+    /* Models */
 
-Unminder.ApplicationController = Ember.ArrayController.extend({
-    createSite: function () {
-        var url = this.get('newSite'),
-            list = this.get('newSiteList');
+    Unminder.Store = DS.Store.extend({
+        revision: 12
+    });
+
+    DS.RESTAdapter.reopen({
+        namespace: 'api'
+    });
+
+
+    Unminder.Site = DS.Model.extend({
+        title: DS.attr('string'),
+        image: DS.attr('string'),
+        url: DS.attr('string'),
+        list: DS.belongsTo('Unminder.List'),
+        backgroundStyle: function () {
+            if (!this.get('image')) {
+                return '';
+            }
+
+            var backgroundImage = this.get('image'),
+                styleString = 'background-image: url(' + backgroundImage + ')';
+            return styleString;
+        }.property('image')
+    });
+
+    Unminder.List = DS.Model.extend({
+        title: DS.attr('string'),
+        sites: DS.hasMany('Unminder.Site')
+    });
+
+    Unminder.User = DS.Model.extend({
+        email: DS.attr('string'),
+        name: DS.attr('string'),
+        password: DS.attr('string')
+    });
+
+    /* Controllers */
+
+    Unminder.ApplicationController = Ember.ArrayController.extend({
+        createSite: function () {
+            var url = this.get('newSite'),
+                list = this.get('newSiteList');
+
             console.log(list);
 
-        if (url.substring(0, 7) !== 'http://') {
-            url = 'http://' + url;
+            if (url.substring(0, 7) !== 'http://') {
+                url = 'http://' + url;
+            }
+
+            Unminder.Site.createRecord({url: url, list: list});
+
+            this.get('store').commit();
+
+            this.set('newSite', '');
+        },
+        createList: function () {
+            var listName = this.get('newList');
+            Unminder.List.createRecord({title: listName});
+
+            this.get('store').commit();
+
+            this.set('newList', '');
+        },
+        deleteList: function (list) {
+            this.get('store').deleteRecord(list);
+            this.get('store').commit();
         }
+    });
 
-        Unminder.Site.createRecord({url: url, list: list});
+    Unminder.SitesController = Ember.ArrayController.extend({
+        delete: function (site) {
+            this.get('store').deleteRecord(site);
+            this.get('store').commit();
+        }
+    });
 
-        this.get('store').commit();
+    Unminder.ListsController = Ember.ObjectController.extend({
+        delete: function (site) {
+            this.get('store').deleteRecord(site);
+            this.get('store').commit();
+        }
+    });
 
-        this.set('newSite', '');
-    },
-    createList: function () {
-        var listName = this.get('newList');
-        Unminder.List.createRecord({title: listName});
+    Unminder.SignupController = Ember.Controller.extend({
+        passwordType: function () {
+            console.log('passwordType!');
+            return this.get('showPassword') ? 'text' : 'password';
+        }.property('showPassword'),
+        signupUser: function () {
+            var newUserHash = {},
+                newUser = {};
 
-        this.get('store').commit();
+            newUserHash.name = this.get('newUserName');
+            newUserHash.email = this.get('newUserEmail');
+            newUserHash.password = this.get('newUserPassword');
 
-        this.set('newList', '');
-    },
-    deleteList: function (list) {
-        this.get('store').deleteRecord(list);
-        this.get('store').commit();
-    }
-});
 
-Unminder.SitesController = Ember.ArrayController.extend({
-    delete: function (site) {
-        this.get('store').deleteRecord(site);
-        this.get('store').commit();
-    }
-});
+            if (this.validateUser(newUserHash)) {
+                newUser = Unminder.User.createRecord(newUserHash);
+                this.get('store').commit();
+            }
 
-Unminder.ListsController = Ember.ObjectController.extend({
-    delete: function (site) {
-        this.get('store').deleteRecord(site);
-        this.get('store').commit();
-    }
-});
+        },
+        validateUser: function (userHash) {
+            var isError = false,
+                nameError = userHash.name.length === 0 ? 'empty' : false,
+                emailError = userHash.email.length === 0 ? 'empty' : false,
+                passwordError = userHash.password.length === 0 ? 'empty' : false;
 
-Unminder.SignupController = Ember.Controller.extend({
-    passwordType: function () {
-        console.log('passwordType!');
-        return this.get('showPassword') ? 'text' : 'password';
-    }.property('showPassword'),
-    namePlaceholder: 'Dave!'
-});
 
-/* Views */
+            this.set('nameError', nameError);
+            this.set('emailError', emailError);
+            this.set('passwordError', passwordError);
 
-Unminder.SiteThumbnail = Ember.View.extend({
-	classNames: ['col-span-4'],
-	tagName: 'div'
-});
+            if (nameError || emailError || passwordError) {
+                return false;
+            }
 
-Unminder.AddSiteButton = Ember.Button.extend(Ember.TargetActionSupport, {
-	classNames: ['btn btn-primary']
-});
+            return true;
+        }
+    });
 
-Unminder.SpinnerView = Ember.View.extend({templateName: 'spinner'});
+    /* Views */
+
+    Unminder.SiteThumbnail = Ember.View.extend({
+        classNames: ['col-span-4'],
+        tagName: 'div'
+    });
+
+    Unminder.AddSiteButton = Ember.Button.extend(Ember.TargetActionSupport, {
+        classNames: ['btn btn-primary']
+    });
+
+    Unminder.SpinnerView = Ember.View.extend({templateName: 'spinner'});
+
+    window.Unminder = Unminder;
+}());
